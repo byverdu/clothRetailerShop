526fd3d515630ddeadf19f4f6810d026f54599b3
byverdu <byverdu@gmail.com>
2014-12-06 21:45:22 +0000

    changed ClothService code structure

--- app/js/service.js
+++ app/js/service.js
@@ -2,87 +2,92 @@
 
 angular.module('shopServices',[])
 	.factory('ClothService',function(){
-			return [	
+			return {
 
-				{
-					name:'Almond Toe Court Shoes, Patent Black',
-					category:'Women’s Footwear',
-					price: '99.0£',
-					stock: 5
-				},
-				{
-					name:'Suede Shoes, Blue' ,
-					category:'Women’s Footwear',
-					price: '42.00£',
-					stock: 4
-				},
-				{
-					name:'Leather Driver Saddle Loafers, Tan' ,
-					category:'Men’s Footwear',
-					price: '34.00£',
-					stock: 12
-				},
-				{
-					name:'Flip Flops, Red' ,
-					category:'Men’s Footwear',
-					price: '19.00£',
-					stock: 6
-				},
-				{
-					name:'Flip Flops, Blue' ,
-					category:'Men’s Footwear',
-					price: '19.00£',
-					stock: 0
-				},
-				{
-					name:'Gold Button Cardigan, Black' ,
-					category:'Women’s Casualwear',
-					price: '167.00£',
-					stock: 6
-				},
-				{
-					name:'Cotton Shorts, Medium Red' ,
-					category:'Women’s Casualwear',
-					price: '30.00£',
-					stock: 5
-				},
-				{
-					name:'Fine Stripe Short Sleeve Shirt, Grey' ,
-					category:'Men’s Casualwear',
-					price: '49.00£',
-					stock: 9
-				},
-				{
-					name:'Fine Stripe Short Sleeve Shirt, Green' ,
-					category:'Men’s Casualwear',
-					price: '39.00£',
-					stock: 3
-				},
-				{
-					name:'Sharkskin Waistcoat,Charcoal' ,
-					category:'Men’s Formalwear',
-					price: '75.00£',
-					stock: 2
-				},
-				{
-					name:'Lightweight Patch Pocket Blazer, Deer' ,
-					category:'Men’s Formalwear',
-					price: '175.50£',
-					stock: 1
-				},
-				{
-					name:'Bird Print Dress, Black' ,
-					category:'Women’s Formalwear',
-					price: '270.00£',
-					stock: 10
-				},
-				{
-					name:'Mid Twist Cut-Out Dress,Pink' ,
-					category:'Women’s Formalwear',
-					price: '540.00£',
-					stock: 5
-				}
-			];
+				sharedCart: [],	
+
+				cloth: [
+
+					{
+						name:'Almond Toe Court Shoes, Patent Black',
+						category:'Women’s Footwear',
+						price: '99.00',
+						stock: 5
+					},
+					{
+						name:'Suede Shoes, Blue' ,
+						category:'Women’s Footwear',
+						price: '42.00',
+						stock: 4
+					},
+					{
+						name:'Leather Driver Saddle Loafers, Tan' ,
+						category:'Men’s Footwear',
+						price: '34.00',
+						stock: 12
+					},
+					{
+						name:'Flip Flops, Red' ,
+						category:'Men’s Footwear',
+						price: '19.00',
+						stock: 6
+					},
+					{
+						name:'Flip Flops, Blue' ,
+						category:'Men’s Footwear',
+						price: '19.00',
+						stock: 0
+					},
+					{
+						name:'Gold Button Cardigan, Black' ,
+						category:'Women’s Casualwear',
+						price: '167.00',
+						stock: 6
+					},
+					{
+						name:'Cotton Shorts, Medium Red' ,
+						category:'Women’s Casualwear',
+						price: '30.00',
+						stock: 5
+					},
+					{
+						name:'Fine Stripe Short Sleeve Shirt, Grey' ,
+						category:'Men’s Casualwear',
+						price: '49.00',
+						stock: 9
+					},
+					{
+						name:'Fine Stripe Short Sleeve Shirt, Green' ,
+						category:'Men’s Casualwear',
+						price: '39.00',
+						stock: 3
+					},
+					{
+						name:'Sharkskin Waistcoat,Charcoal' ,
+						category:'Men’s Formalwear',
+						price: '75.00',
+						stock: 2
+					},
+					{
+						name:'Lightweight Patch Pocket Blazer, Deer' ,
+						category:'Men’s Formalwear',
+						price: '175.50',
+						stock: 1
+					},
+					{
+						name:'Bird Print Dress, Black' ,
+						category:'Women’s Formalwear',
+						price: '270.00',
+						stock: 10
+					},
+					{
+						name:'Mid Twist Cut-Out Dress,Pink' ,
+						category:'Women’s Formalwear',
+						price: '540.00',
+						stock: 5
+					}
+			]
+		};
 	});
 
 	
--- test/unit/clothServiceSpec.js
+++ test/unit/clothServiceSpec.js
@@ -6,7 +6,7 @@ describe('ClothService', function() {
 	var fakeCloth = {
 		name: 'Almond Toe Court Shoes, Patent Black',
 		category: 'Women’s Footwear',
-		price: '99.0£',
+		price: '99.00',
 		stock: 5
 	};
 	
@@ -20,26 +20,34 @@ describe('ClothService', function() {
 
 	it('should be defined', function() {
 		expect(service).not.to.be.undefined;
-		expect(service).to.be.an('array');
+		expect(service).to.be.an('object');
 	});
 
-	it('should contain all the cloth stock', function() {
-		expect(service).to.contain(fakeCloth).and.have.length(13);
+	it('should have a cloth property', function() {
+		expect(service).to.have.property('cloth').that.is.an('array');
 	});
 
-	it('each cloth has a property name', function() {
-		expect(service[0]).to.have.property('name').to.eq('Almond Toe Court Shoes, Patent Black');
+	it('that should contain all the cloth stock', function() {
+		expect(service.cloth).to.contain(fakeCloth).and.have.length(13);
 	});
 
-	it('each cloth has a property category', function() {
-		expect(service[0]).to.have.property('category').to.eq('Women’s Footwear');
+	it('and each cloth has a property name', function() {
+		expect(service.cloth[0]).to.have.property('name').to.eq('Almond Toe Court Shoes, Patent Black');
 	});
 
-	it('each cloth has a property price', function() {
-		expect(service[0]).to.have.property('price').to.eq('99.0£');
+	it('and each cloth has a property category', function(){
+		expect(service.cloth[0]).to.have.property('category').to.eq('Women’s Footwear');
 	});
 
-	it('each cloth has a property stock', function() {
-		expect(service[0]).to.have.property('stock').to.eq(5);
+	it('and each cloth has a property price', function() {
+		expect(service.cloth[0]).to.have.property('price').to.eq('99.00');
+	});
+
+	it('and each cloth has a property stock', function() {
+		expect(service.cloth[0]).to.have.property('stock').to.eq(5);
+	});
+
+	it('should have a property that contains the selected products', function() {
+		expect(service).to.have.property('sharedCart').that.is.an('array');
 	});
 });
